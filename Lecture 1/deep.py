f5713299704c2f1fa43c8794388063623213be73
Thomas Lincoln <thmas5952@gmail.com>
2023-11-02 12:43:35 -0300

    :Revert "Lecture 1"
    This reverts commit 80e980d2aa21299ad3fee5574c3841490f6241ff.

--- Lecture 1/deep.py
+++
@@ -1,6 +0,0 @@
-resposta = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
-
-if resposta == "42" or resposta == "forty-two" or resposta == "forty two":
-    print("Yes")
-else: 
-    print("No")
