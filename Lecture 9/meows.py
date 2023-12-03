44f805afb30642ca2102ddbbff6f68d28b4161b3
Thomas Lincoln <thmas5952@gmail.com>
2023-12-03 12:23:40 -0300

    feat: :sparkles: Add all examples of Lecture 9

--- Lecture 9/meows.py
+++ Lecture 9/meows.py
@@ -1,9 +1,45 @@
-class Cat:
-    MEOWS = 3
+# class Cat:
+#     MEOWS = 3
     
-    def meow(self):
-        for _ in range(Cat.MEOWS):
-            print("meow")
+#     def meow(self):
+#         for _ in range(Cat.MEOWS):
+#             print("meow")
 
-cat = Cat()
-cat.meow()
+# cat = Cat()
+# cat.meow()
+
+# Docstring
+# def meow(n: int):
+#     """
+#     Meow n times.
+    
+#     :param n: Number of times do meow
+#     :type n: int
+#     """
+#     return "meow\n" * n
+
+# number: int = input("Number: ")
+# meows: str = meow(number)
+# print(meows, end="")
+#----------------------------------------
+
+
+# import sys
+
+# if len(sys.argv) == 1:
+#     print("meow")
+# elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+#     n = int(sys.argv[2])
+#     for _ in range(n):
+#         print("meow")
+# else:
+#     print("usage: meows.py")
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="Number of times to meow", type=int)
+args = parser.parse_args()
+
+for _ in range(int(args.n)):
+  print("meow")
